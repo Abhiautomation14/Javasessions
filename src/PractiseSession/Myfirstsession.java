34d1d6f56102d0a6e329a01429b7db24094cbaaa
Abhi <abhi04bn@gmail.com>
2024-12-18 09:48:41 +0530

    java practise

---
+++ src/PractiseSession/Myfirstsession.java
@@ -0,0 +1,27 @@
+package PractiseSession;
+
+public class Myfirstsession {
+	
+	void add1()
+	{
+		System.out.println("Welcome to add");
+		}
+	void sub1()
+	{
+		System.out.println("Welcome to sub");
+	}
+	void mul()
+	{
+		System.out.println("Welcome to mul");
+	}
+
+	public static void main(String[] args) {
+		Myfirstsession obj=new Myfirstsession();
+		obj.add1();
+		obj.sub1();
+		obj.mul();
+		
+
+	}
+
+}
